8686d71259c737e61d11ae1a82cd3e4a6094ce05
Mehranai <mehran.shmohamadi@gmail.com>
2025-11-15 10:17:26 +0330

    Update 1

--- test_neo_connection.py
+++ test_neo_connection.py
@@ -1,6 +1,6 @@
 from neo4j import GraphDatabase
 
-driver = GraphDatabase.driver("neo4j://127.0.0.1:7687", auth=("neo4j", "mehran.boxer8"))
+driver = GraphDatabase.driver("neo4j://127.0.0.1:7687", auth=("neo4j", "mehran11"))
 
 with driver.session(database="task1") as session:
     result = session.run("RETURN 'Connected to task1!' AS msg")
